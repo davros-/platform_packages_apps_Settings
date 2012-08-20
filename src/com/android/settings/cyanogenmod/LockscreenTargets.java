fde1db3f634db83614edcf377b7aa211cf767e12
davros <davros@droidconcepts.com>
2012-08-20 11:02:57 -0400

    Settings : Fix isPhone reference
    Add missing argument to method call.
    
    Change-Id: I51f46a1be166073dfb5c743b14fc994cff9b3ba5
    
    Conflicts:
    
    	src/com/android/settings/cyanogenmod/LockscreenTargets.java

--- src/com/android/settings/cyanogenmod/LockscreenTargets.java
+++ src/com/android/settings/cyanogenmod/LockscreenTargets.java
@@ -107,6 +107,7 @@ public class LockscreenTargets extends Fragment implements ShortcutPickHelper.On
         setHasOptionsMenu(true);
         mIsScreenLarge = Utils.isScreenLarge();
         mActivity = getActivity();
+        mIsScreenLarge = !Utils.isPhone(mActivity);
         mResources = getResources();
         mIsLandscape = mResources.getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
         mTargetOffset = mIsLandscape && !mIsScreenLarge ? 2 : 0;
