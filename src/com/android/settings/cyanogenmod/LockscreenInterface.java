067dabff004bec90f2845d0fa1e73b5c02b920b8
davros <davros@droidconcepts.com>
2012-09-01 01:49:21 -0400

    Lockscreen Vibrate option 2 of 2
    Add the new Setting Settings.System.LOCKSCREEN_VIBRATE_ENABLED labeled in English as
    
    Vibrate
    Vibrate when unlocking
    (checkbox)
    
    This Setting defaults to Checked.
    
    Change-Id: I95c0dac1c06ad3ab6d1aef34a7dc21cbe9236e36
    
    Conflicts:
    
    	res/values/strings.xml
    	res/xml/lockscreen_interface_settings.xml
    	src/com/android/settings/cyanogenmod/LockscreenInterface.java

--- src/com/android/settings/cyanogenmod/LockscreenInterface.java
+++ src/com/android/settings/cyanogenmod/LockscreenInterface.java
@@ -30,6 +30,7 @@ import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
@@ -51,7 +52,10 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
     public static final String KEY_WEATHER_PREF = "lockscreen_weather";
     public static final String KEY_CALENDAR_PREF = "lockscreen_calendar";
     public static final String KEY_BACKGROUND_PREF = "lockscreen_background";
+    public static final String KEY_VIBRATE_PREF = "lockscreen_vibrate";
+    private static final String KEY_ALWAYS_BATTERY_PREF = "lockscreen_battery_status";
     private ListPreference mCustomBackground;
+    private CheckBoxPreference mVibratePref;
     private Preference mWeatherPref;
     private Preference mCalendarPref;
     private Activity mActivity;
@@ -70,11 +74,24 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
         addPreferencesFromResource(R.xml.lockscreen_interface_settings);
         mWeatherPref = (Preference) findPreference(KEY_WEATHER_PREF);
         mCalendarPref = (Preference) findPreference(KEY_CALENDAR_PREF);
+
         mCustomBackground = (ListPreference) findPreference(KEY_BACKGROUND_PREF);
         mCustomBackground.setOnPreferenceChangeListener(this);
         mIsScreenLarge = Utils.isScreenLarge();
         wallpaperImage = new File(mActivity.getFilesDir()+"/lockwallpaper");
         wallpaperTemporary = new File(mActivity.getCacheDir()+"/lockwallpaper.tmp");
+
+        mBatteryStatus = (ListPreference) findPreference(KEY_ALWAYS_BATTERY_PREF);
+        mBatteryStatus.setOnPreferenceChangeListener(this);
+
+        mVibratePref = (CheckBoxPreference) findPreference(KEY_VIBRATE_PREF);
+        boolean bVibrate = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_VIBRATE_ENABLED, 1) == 1 ? true : false;
+        mVibratePref.setChecked(bVibrate);
+        mVibratePref.setOnPreferenceChangeListener(this);
+
+        mIsScreenLarge = Utils.isTablet(getActivity());
+
         updateCustomBackgroundSummary();
     }
 
@@ -242,6 +259,25 @@ public class LockscreenInterface extends SettingsPreferenceFragment implements
                 break;
             }
             return true;
+<<<<<<< HEAD
+=======
+        } else if (preference == mBatteryStatus) {
+            int value = Integer.valueOf((String) objValue);
+            int index = mBatteryStatus.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_ALWAYS_SHOW_BATTERY, value);
+            mBatteryStatus.setSummary(mBatteryStatus.getEntries()[index]);
+            return true;
+        } else if (preference == mVibratePref) {
+            boolean bValue = Boolean.valueOf((Boolean) objValue);
+            int value = 0;
+            if (bValue) {
+                value = 1;
+            }
+            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
+                    Settings.System.LOCKSCREEN_VIBRATE_ENABLED, value);
+            return true;
+>>>>>>> 157ec04... Lockscreen Vibrate option 2 of 2
         }
         return false;
     }
